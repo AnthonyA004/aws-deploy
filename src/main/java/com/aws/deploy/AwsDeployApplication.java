970b131debf8f038fbc9cdee808fcdf8ee3a0cf3
AnthonyA004 <aauquitorres@gmail.com>
2026-01-13 10:55:48 -0500

    Initial commit - Spring Boot AWS deploy

---
+++ src/main/java/com/aws/deploy/AwsDeployApplication.java
@@ -0,0 +1,11 @@
+package com.aws.deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+public class AwsDeployApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AwsDeployApplication.class, args);
+    }
+
+}
